acaced356325be4cb3e7f6f905611df37f854645
chaimaaenaa <mekayssichaimaaenaa@gmail.com>
2024-08-03 01:06:24 +0100

    start project IT

---
+++ src/main/java/com/itsolutions/ItsolutionsApplication.java
@@ -0,0 +1,13 @@
+package com.itsolutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItsolutionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItsolutionsApplication.class, args);
+	}
+
+}
