54cea843a15948b315de601ab88bcc49737740d7
chaimaaenaa <mekayssichaimaaenaa@gmail.com>
2024-08-05 14:41:18 +0100

    create les component

--- frontend_SI/src/app/app.module.ts
+++ frontend_SI/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UtilisateurComponent } from './utilisateur/utilisateur.component';
+import { TechnicienComponent } from './technicien/technicien.component';
+import { EquipmentComponent } from './equipment/equipment.component';
+import { PanneComponent } from './panne/panne.component';
+import { TicketSupportComponent } from './ticket-support/ticket-support.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UtilisateurComponent,
+    TechnicienComponent,
+    EquipmentComponent,
+    PanneComponent,
+    TicketSupportComponent
   ],
   imports: [
     BrowserModule,
